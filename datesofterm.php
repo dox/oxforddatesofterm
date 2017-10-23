eb1d3f052b4744be49adba6abcde8473ef2754fb
Andrew Breakspear <andrew@longhanborough.com>
2017-10-23 10:59:47 +0100

    Update datesofterm.php

--- datesofterm.php
+++ datesofterm.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Oxford Dates of Term
- * Version 2014-01
+ * Version 2017-10
  *
  * A small function that will pass a date ('YYYY-mm-dd') and
  * providing that date falls within an Oxford term, it will 
@@ -23,27 +23,45 @@ function ox_term_date($compareDate = NULL) {
 	}
 	
 	// lookup array of Oxford term dates in the format 'start_date,_end_date,term_name'
+	//2011-2012
 	$term[] = array("2011-10-09","2011-12-03","Michaelmas");
 	$term[] = array("2012-01-15","2012-03-10","Hilary");
 	$term[] = array("2012-04-22","2012-06-16","Trinity");
+	
+	//2012-2013
 	$term[] = array("2012-10-07","2012-12-01","Michaelmas");
 	$term[] = array("2013-01-13","2013-03-09","Hilary");
 	$term[] = array("2013-04-21","2013-06-15","Trinity");
+	
+	//2013-2014
 	$term[] = array("2013-10-13","2013-12-07","Michaelmas");
 	$term[] = array("2014-01-19","2014-03-15","Hilary");
 	$term[] = array("2014-04-27","2014-06-21","Trinity");
+	
+	//2014-2015
 	$term[] = array("2014-10-12","2014-12-06","Michaelmas");
 	$term[] = array("2015-01-18","2015-03-14","Hilary");
 	$term[] = array("2015-04-26","2015-06-20","Trinity");
+	
+	//2015-2016
 	$term[] = array("2015-10-11","2015-12-05","Michaelmas");
 	$term[] = array("2016-01-17","2016-03-12","Hilary");
 	$term[] = array("2016-04-24","2016-06-18","Trinity");
+	
+	//2016-2017
 	$term[] = array("2016-10-09","2016-12-03","Michaelmas");
 	$term[] = array("2017-01-15","2017-03-11","Hilary");
 	$term[] = array("2017-04-23","2017-06-17","Trinity");
+	
+	//2017-2018
 	$term[] = array("2017-10-08","2017-12-02","Michaelmas");
 	$term[] = array("2018-01-14","2018-03-10","Hilary");
-	$term[] = array("2018-04-22","2019-06-16","Trinity");
+	$term[] = array("2018-04-22","2018-06-16","Trinity");
+	
+	//2018-2019
+	$term[] = array("2018-10-07","2018-12-01","Michaelmas");
+	$term[] = array("2019-01-13","2019-03-09","Hilary");
+	$term[] = array("2019-04-28","2010-06-22","Trinity");
 	
 	foreach($term AS $date) {
 		if (check_in_range($date[0], $date[1], $compareDate)) {
@@ -66,7 +84,7 @@ function ox_term_date($compareDate = NULL) {
 	}
 	
 	if ($foundTerm['term_name'] == NULL) {
-		$foundTerm['term_name'] = "Vacation";
+		$foundTerm['term_name'] = "Holiday";
 	}
 	
 	return $foundTerm;
@@ -82,7 +100,4 @@ function check_in_range($start_date, $end_date, $date_from_user) {
 	return (($user_ts >= $start_ts) && ($user_ts <= $end_ts));
 }
 
-echo "<pre>";
-print_r(ox_term_date('2014-04-28'));
-echo "</pre>";
 ?>
